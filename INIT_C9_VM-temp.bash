288019a479f6019c4f6459822e4e9f943049e2c9
KoreaHaos <benjaminhaos@ewha.ac.kr>
2016-06-11 06:20:57 +0000

    test

---
+++ INIT_C9_VM-temp.bash
@@ -0,0 +1,9 @@
+# A fresh VM always needs a little setup
+
+# ToDo : Figure out how to setup the gitnor to not track the removal of files.
+# ToDo : Have it delete itself.
+
+# This line changes the bash history.
+
+bash etc/z_/hack_the_bash_history.bash
+
