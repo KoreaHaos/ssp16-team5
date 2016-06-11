e88fb34c562c777c89d4eb7ce3865536124d8763
KoreaHaos <benjaminhaos@ewha.ac.kr>
2016-06-11 06:30:06 +0000

    Script commit from Cloud9 VM(C9).C9 user : KoreaHaos. Committing script name :  Script committing function name : create_cloud9_manage_git_directories_script_commit.

--- INIT_C9_VM-temp.bash
+++ INIT_C9_VM-temp.bash
@@ -3,7 +3,10 @@
 # ToDo : Figure out how to setup the gitnor to not track the removal of files.
 # ToDo : Have it delete itself.
 
-# This line changes the bash history.
+# Make VM cacke git creds
+time_to_cache_dits_for="86400"
+git config --global credential.helper "cache --timeout=$time_to_cache_dits_for"
 
+# This line changes the bash history.
 bash etc/z_/hack_the_bash_history.bash
 
