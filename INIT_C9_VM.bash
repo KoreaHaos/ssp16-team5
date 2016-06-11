30b2909cfbc377e5bec6d5e4871ce6129976b0ed
KoreaHaos <benjaminhaos@ewha.ac.kr>
2016-06-11 07:36:08 +0000

    Script commit from Cloud9 VM(C9).C9 user : KoreaHaos. Committing script name :  Script committing function name : create_cloud9_manage_git_directories_script_commit.

--- INIT_C9_VM.bash
+++ INIT_C9_VM.bash
@@ -5,7 +5,20 @@
 
 # This line changes the bash history.
 
-
+function c_cache_git_creds_for_x_minutes() {
+    local num_minutes
+    
+    if [ -z "$1" ]
+    then
+        read -p "Time in minutes to cashe the dits? : " num_minutes;
+    else
+        num_minutes="$1"
+    fi
+    
+    # ToDo : Figure out how to display the timeout value and echo it back!
+    let "num_minutes = $num_minutes * 60"
+    git config --global credential.helper "cache --timeout=$num_minutes"
+}
 
 function make_history_change() {
     bash "$HISTORY_CHANGING__PATH_AND_FILENAME"
@@ -15,6 +28,8 @@ function setup_changes_in_history() {
     THIS_SCRIPTS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
     HISTORY_CHANGING__PATH_AND_FILENAME="$THIS_SCRIPTS_DIR/etc/z_/hack_the_bash_history.bash"
 }
+
+c_cache_git_creds_for_x_minutes "1440"
 
 setup_changes_in_history
 make_history_change
