4b06e7e8290b16d1bca8bc68943321ee3b9c9375
KoreaHaos <benjaminhaos@ewha.ac.kr>
2016-06-12 14:26:42 +0000

    Script commit from Cloud9 VM(C9).C9 user : KoreaHaos. Committing script name :  Script committing function name : create_cloud9_manage_git_directories_script_commit.

---
+++ runme_for_links.bash
@@ -0,0 +1,13 @@
+
+echo"
+https://ide.c9.io/koreahaos/finalize_presentor_ssp
+https://codebender.cc/sketch:327732
+http://ai2.appinventor.mit.edu/#5765309816635392
+"
+
+
+echo "The GitHub-Pages [index](http://koreahaos.github.io/ssp16-team5/) file."
+echo "CodeBender (https://codebender.cc/home)"
+echo "The Reveal.js [slides](http://koreahaos.github.io/ssp16-team5/doc/prsent/reveals_slides/final_present.html) hosted on GitHub-Pages."
+echo "The GitHub [Repo](https://github.com/KoreaHaos/ssp16-team5)"
+
